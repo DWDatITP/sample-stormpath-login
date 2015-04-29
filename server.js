5516aeb065b0beeb01da4a64407405bd2902f683
Cory Forsyth <cory.forsyth@gmail.com>
2015-04-29 18:31:01 -0400

    remove stormpath stuff for now

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
-var stormpath = require('express-stormpath');
-
-app.use(stormpath.init(app, {
-  apiKeyId: process.env.STORMPATH_API_KEY_ID,
-  apiKeySecret: process.env.STORMPATH_API_KEY_SECRET,
-  secretKey:    process.env.STORMPATH_SECRET_KEY,
-  application:  process.env.STORMPATH_URL
-}));
 
 app.get('/', function(req, res){
+  console.log('req.user:',req.user);
   if (req.user) {
     console.log('logged in as req.user: ',req.user);
     res.send('You are logged in as : ' + req.user.username);
