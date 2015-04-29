0ac4f934557c45bd09fa15acdc9cb24eb12b266a
Cory Forsyth <cory.forsyth@gmail.com>
2015-04-29 19:16:48 -0400

    change server

--- server.js
+++ server.js
@@ -2,14 +2,9 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 
-app.get('/', function(req, res){
-  console.log('req.user:',req.user);
-  if (req.user) {
-    console.log('logged in as req.user: ',req.user);
-    res.send('You are logged in as : ' + req.user.username);
-  } else {
-    res.send('Visit <a href="/login">/login</a> or <a href="/register">/register</a>.');
-  }
+app.get('/', function)req, res){
+  var world = 'World';
+  res.send('Hello ' + world);
 });
 
 app.listen(port, function(){
