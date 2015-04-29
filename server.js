4a7aede778aeba017387067ebe863a034bc418d7
Cory Forsyth <cory.forsyth@gmail.com>
2015-04-29 19:43:21 -0400

    add stormpath

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
+var stormpath = require('express-stormpath');
 var app = express();
 var port = process.env.PORT || 5000;
 
-app.get('/', function)req, res){
+app.use(stormpath.init(app, {
+  apiKeyId:     process.env.STORMPATH_API_KEY_ID,
+  apiKeySecret: process.env.STORMPATH_API_KEY_SECRET,
+  secretKey:    process.env.STORMPATH_SECRET_KEY,
+  application:  process.env.STORMPATH_URL,
+}));
+
+app.get('/', function(req, res){
   var world = 'World';
   res.send('Hello ' + world);
 });
